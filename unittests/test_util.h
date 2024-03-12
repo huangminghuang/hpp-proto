fe5af850d9047f82940fa7d612ae6d224d8c0f96
Huang-Ming Huang <huangming.huang@gmail.com>
2024-03-12 09:18:09 -0500

    some refactor

--- unittests/test_util.h
+++ unittests/test_util.h
@@ -1,37 +1,12 @@
 
 #pragma once
-
-#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <span>
 #include <algorithm>
 #include <ranges>
 #include <hpp_proto/pb_serializer.h>
-
-inline std::string descriptorset_from_file(const char* filename) {
-  std::ifstream in(filename, std::ios::in | std::ios::binary);
-  std::string contents;
-  in.seekg(0, std::ios::end);
-  contents.resize(in.tellg());
-  in.seekg(0, std::ios::beg);
-  in.read(&contents[0], contents.size());
-  return contents;
-}
-
-template <typename T>
-std::string to_hex(const T &data) {
-  static const char qmap[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-  std::string result;
-  result.resize(data.size() * 2);
-  int index = 0;
-  for (auto b : data) {
-    unsigned char c = static_cast<unsigned char>(b);
-    result[index++] = qmap[c >> 4];
-    result[index++] = qmap[c & '\x0F'];
-  }
-  return result;
-}
 
 namespace std {
 inline std::ostream &operator<<(std::ostream &os, std::byte b) {
@@ -76,12 +51,3 @@ struct monotonic_buffer_resource {
   }
 };
 
-template <typename RG1, typename RG2>
-bool ranges_equal(RG1 &&r1, RG2 &&r2) {
-  return std::equal(r1.begin(), r1.end(), r2.begin(), r2.end());
-}
-
-template <typename RG1, typename RG2, typename BinaryPredicate>
-bool ranges_equal(RG1 &&r1, RG2 &&r2, BinaryPredicate p) {
-  return std::equal(r1.begin(), r1.end(), r2.begin(), r2.end(), p);
-}
