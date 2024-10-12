e412c1a3eafb6f57241670700f3f8e05558a4fe3
Huang-Ming Huang <huangming.huang@gmail.com>
2024-10-12 15:53:27 -0500

    windows fix

--- third-party.cmake
+++ third-party.cmake
@@ -6,7 +6,7 @@ CPMAddPackage(
     NAME glaze
     GIT_TAG v2.9.5
     GITHUB_REPOSITORY stephenberry/glaze
-    PATCH_COMMAND ${GIT_EXECUTABLE} apply ${CMAKE_CURRENT_SOURCE_DIR}/glaze-2.9.5.patch
+    PATCH_COMMAND ${GIT_EXECUTABLE} apply --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_SOURCE_DIR}/glaze-2.9.5.patch
     UPDATE_DISCONNECTED ON
 )
 
