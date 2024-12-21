e956dea48149b04966d2d4d9f762643cb3b75a8f
Huang-Ming Huang <huangming.huang@gmail.com>
2024-12-21 12:17:16 -0600

    try zero-sized array

--- include/hpp_proto/pb_serializer.hpp
+++ include/hpp_proto/pb_serializer.hpp
@@ -817,9 +817,9 @@ struct reverse_indices {
     requires requires { T::number; }
   constexpr static auto get_numbers(T meta) {
     if constexpr (meta.number != UINT32_MAX) {
-      return std::array<uint32_t, 1>{meta.number};
+      return std::array<std::uint32_t, 1>{meta.number};
     } else {
-      return std::span<uint32_t>{};
+      return std::array<std::uint32_t, 0>{};
     }
   }
 
