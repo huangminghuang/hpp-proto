e1581d837ed15a56d9b7d55da9fccde8d8a4f42c
Clang Robot <huangming.huang@gmail.com>
2025-01-17 14:22:43 +0000

    :art: Committing clang-format changes

--- fuzz/fuzz_case.cpp
+++ fuzz/fuzz_case.cpp
@@ -1,6 +1,5 @@
-#include <google/protobuf/unittest_proto3.pb.hpp>
 #include <fstream>
-
+#include <google/protobuf/unittest_proto3.pb.hpp>
 
 inline std::string read_file(const char *filename) {
   std::ifstream in(filename, std::ios::in | std::ios::binary);
@@ -12,7 +11,7 @@ inline std::string read_file(const char *filename) {
   return contents;
 }
 
-int main(int argc, const char** argv) {
+int main(int argc, const char **argv) {
   std::string data = read_file(argv[1]);
   proto3_unittest::TestAllTypes message;
   auto status = hpp::proto::read_proto(message, data);
