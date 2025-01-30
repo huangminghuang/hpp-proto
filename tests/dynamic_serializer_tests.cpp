5e7f24c959bfe193cdbed556bcc1fa8bac580acd
Clang Robot <huangming.huang@gmail.com>
2025-01-30 22:13:49 +0000

    :art: Committing clang-format changes

--- tests/dynamic_serializer_tests.cpp
+++ tests/dynamic_serializer_tests.cpp
@@ -6,42 +6,44 @@
 using namespace boost::ut;
 
 const boost::ut::suite dynamic_serializer_test = [] {
-  "unit"_test = [](const std::string &message_name) {
-    using namespace boost::ut::literals;
+  "unit"_test =
+      [](const std::string &message_name) {
+        using namespace boost::ut::literals;
 
-    using namespace std::string_literals;
-    std::string data = read_file("data/"s + message_name + ".binpb");
+        using namespace std::string_literals;
+        std::string data = read_file("data/"s + message_name + ".binpb");
 
-    auto descriptors = read_file("unittest.desc.binpb");
-    auto ser = hpp::proto::dynamic_serializer::make(descriptors);
-    expect(fatal(ser.has_value()));
+        auto descriptors = read_file("unittest.desc.binpb");
+        auto ser = hpp::proto::dynamic_serializer::make(descriptors);
+        expect(fatal(ser.has_value()));
 
-    auto gpb_result = read_file("data/"s + message_name + ".json");
-    expect(fatal(!gpb_result.empty()));
+        auto gpb_result = read_file("data/"s + message_name + ".json");
+        expect(fatal(!gpb_result.empty()));
 
-    auto hpp_result = ser->proto_to_json(message_name, data);
-    expect(fatal(hpp_result.has_value()));
+        auto hpp_result = ser->proto_to_json(message_name, data);
+        expect(fatal(hpp_result.has_value()));
 
-    expect(eq(gpb_result, *hpp_result));
+        expect(eq(gpb_result, *hpp_result));
 
-    std::string serialized;
-    expect(fatal(ser->json_to_proto(message_name, *hpp_result, serialized).ok()));
-    expect(eq(to_hex(data), to_hex(serialized)));
+        std::string serialized;
+        expect(fatal(ser->json_to_proto(message_name, *hpp_result, serialized).ok()));
+        expect(eq(to_hex(data), to_hex(serialized)));
 
-    // NOLINTBEGIN(readability-implicit-bool-conversion)
-    hpp_result = ser->proto_to_json(message_name, data, hpp::proto::indent_level<3>);
-    // NOLINTEND(readability-implicit-bool-conversion)
-    expect(fatal(hpp_result.has_value()));
-    expect(ser->json_to_proto(message_name, *hpp_result, serialized).ok());
-    expect(eq(to_hex(data), to_hex(serialized)));
-  } | std::vector<std::string>{
-    // "proto3_unittest.TestAllTypes",       "proto3_unittest.TestUnpackedTypes",
-    //                            "protobuf_unittest.TestAllTypes",     "protobuf_unittest.TestMap",
-                               "protobuf_unittest.TestPackedTypes",
+        // NOLINTBEGIN(readability-implicit-bool-conversion)
+        hpp_result = ser->proto_to_json(message_name, data, hpp::proto::indent_level<3>);
+        // NOLINTEND(readability-implicit-bool-conversion)
+        expect(fatal(hpp_result.has_value()));
+        expect(ser->json_to_proto(message_name, *hpp_result, serialized).ok());
+        expect(eq(to_hex(data), to_hex(serialized)));
+      } |
+      std::vector<std::string>{
+          // "proto3_unittest.TestAllTypes",       "proto3_unittest.TestUnpackedTypes",
+          //                            "protobuf_unittest.TestAllTypes",     "protobuf_unittest.TestMap",
+          "protobuf_unittest.TestPackedTypes",
 #ifdef EDITION_SUPPORT
-                               "protobuf_unittest.TestAllTypesLite", "protobuf_unittest.TestPackedTypesLite",
+          "protobuf_unittest.TestAllTypesLite", "protobuf_unittest.TestPackedTypesLite",
 #endif
-                               "protobuf_unittest.TestUnpackedTypes"};
+          "protobuf_unittest.TestUnpackedTypes"};
 };
 
 const boost::ut::suite dynamic_serializer_skip_test = [] {
@@ -49,12 +51,12 @@ const boost::ut::suite dynamic_serializer_skip_test = [] {
   auto ser = hpp::proto::dynamic_serializer::make(descriptors);
   expect(fatal(ser.has_value()));
   std::string data = read_file("data/proto3_unittest.TestAllTypes.binpb");
-  
+
   std::array<char, 2> sbytes, ebytes;
   hpp::proto::unchecked_pack_varint(hpp::proto::make_tag(200, hpp::proto::wire_type::sgroup), sbytes.data());
   hpp::proto::unchecked_pack_varint(hpp::proto::make_tag(200, hpp::proto::wire_type::egroup), ebytes.data());
   std::ranges::copy(sbytes, std::inserter(data, data.begin()));
-  std::ranges::copy(ebytes, std::back_inserter(data));  
+  std::ranges::copy(ebytes, std::back_inserter(data));
   std::ranges::copy(read_file("data/protobuf_unittest.TestAllTypes.binpb"), std::back_inserter(data));
 
   auto hpp_result = ser->proto_to_json("protobuf_unittest.TestAllTypes", data);
