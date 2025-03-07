c11b1595e8b83b7be5760f2d5ad3f1160298d29e
Huang-Ming Huang <huangming.huang@gmail.com>
2025-03-07 13:56:52 -0600

    disable clang-tidy fmt

--- third-parties.cmake
+++ third-parties.cmake
@@ -27,9 +27,10 @@ get_target_property(IS_UTF8_COMPILER_OPTIONS is_utf8 COMPILE_OPTIONS)
 if(IS_UTF8_COMPILER_OPTIONS MATCHES "fsanitize=address")
     message(FATAL_ERROR "is_utf8 is not compatible with address sanitizer")
 endif()
-set_target_properties(is_utf8 PROPERTIES CXX_CLANG_TIDY "")
 
 CPMAddPackage("gh:fmtlib/fmt#10.1.0")
+set_target_properties(is_utf8 fmt PROPERTIES CXX_CLANG_TIDY "")
+
 
 if(HPP_PROTO_PROTOC STREQUAL "find")
     find_package(Protobuf CONFIG)
