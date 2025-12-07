85c4d6b9ff4478366a0046a3968d1d36beee719d
Clang Robot <huangming.huang@gmail.com>
2025-12-07 16:43:36 +0000

    :art: Committing clang-format changes

--- include/hpp_proto/descriptor_pool.hpp
+++ include/hpp_proto/descriptor_pool.hpp
@@ -283,8 +283,7 @@ public:
     [[nodiscard]] bool is_closed() const { return options_.features.value().enum_type == FeatureSet::EnumType::CLOSED; }
 
     [[nodiscard]] bool valid_enum_value(int32_t v) const {
-      return !is_closed() ||
-             std::ranges::contains(proto_.value, v, [](const auto &item) { return item.number; });
+      return !is_closed() || std::ranges::contains(proto_.value, v, [](const auto &item) { return item.number; });
     }
 
   private:
