68ab00dd9aa128bd233d1c89cb8903626e02a6bf
Huang-Ming Huang <huangming.huang@gmail.com>
2026-01-21 09:18:52 -0600

    add lsan_ignoring_resource to manage memory allocation with AddressSanitizer support

--- tutorial/pmr/pmr_tutorial_proto3.cpp
+++ tutorial/pmr/pmr_tutorial_proto3.cpp
@@ -6,9 +6,39 @@
 #include "addressbook_proto3.glz.hpp"
 #include "addressbook_proto3.pb.hpp"
 
+#if defined(__has_include)
+#if __has_include(<sanitizer/lsan_interface.h>)
+#if defined(__SANITIZE_ADDRESS__)
+#include <sanitizer/lsan_interface.h>
+#define HPP_PROTO_HAS_LSAN 1
+#endif
+#endif
+#endif
+
 using PmrAddressBook = tutorial::AddressBook<hpp::proto::pmr_traits>;
 using PmrPerson = tutorial::Person<hpp::proto::pmr_traits>;
 using PmrPhoneNumber = PmrPerson::PhoneNumber;
+
+struct lsan_ignoring_resource final : std::pmr::memory_resource {
+  explicit lsan_ignoring_resource(std::pmr::memory_resource *upstream) : upstream_(upstream) {}
+
+private:
+  void *do_allocate(std::size_t bytes, std::size_t alignment) override {
+    void *ptr = upstream_->allocate(bytes, alignment);
+#if defined(HPP_PROTO_HAS_LSAN)
+    __lsan_ignore_object(ptr);
+#endif
+    return ptr;
+  }
+
+  void do_deallocate(void *ptr, std::size_t bytes, std::size_t alignment) override {
+    upstream_->deallocate(ptr, bytes, alignment);
+  }
+
+  bool do_is_equal(const std::pmr::memory_resource &other) const noexcept override { return this == &other; }
+
+  std::pmr::memory_resource *upstream_;
+};
 
 inline void expect(bool condition, const std::source_location location = std::source_location::current()) {
   if (!condition) {
@@ -20,16 +50,18 @@ inline void expect(bool condition, const std::source_location location = std::so
 int main() {
   // Create a monotonic buffer resource on the stack
   std::array<std::byte, 4096> buffer{};
-  std::pmr::monotonic_buffer_resource pool{buffer.data(), buffer.size()};
+  lsan_ignoring_resource lsan_resource{std::pmr::get_default_resource()};
+  std::pmr::monotonic_buffer_resource pool{buffer.data(), buffer.size(), &lsan_resource};
+
+  // This is the only way to propagate the memory resource to nested objects.
+  // Do not change the default resource until all mutations are complete.
+  std::pmr::set_default_resource(&pool);
+
   std::pmr::polymorphic_allocator<> alloc{&pool};
 
   // Create an address book that allocates from the pool and deliberately skip its destructor
   // to avoid unnecessary overhead.
   auto *address_book = alloc.new_object<PmrAddressBook>();
-
-  // This is the only way to propagate the memory resource to nested objects.
-  // Do not change the default resource until all mutations are complete.
-  std::pmr::set_default_resource(&pool);
 
   // Add a person
   address_book->people.emplace_back(
