56b6af6efcbed5c2313f183c8c09d1c52a177a6c
Clang Robot <huangming.huang@gmail.com>
2026-01-31 05:43:49 +0000

    :art: Committing clang-format changes

--- fuzz/fuzz_binpb.cpp
+++ fuzz/fuzz_binpb.cpp
@@ -88,8 +88,7 @@ extern "C" __attribute__((visibility("default"))) int LLVMFuzzerTestOneInput(con
             auto owning_write = round_trip_test(owning_message, owning_message_t{});
             auto dyn_write = round_trip_test(dyn_message, factory.get_message(msg_name, mr).value());
 
-            if (msg_name != "protobuf_unittest.TestMap"sv &&
-                msg_name != "proto2_unittest.TestWellKnownTypes"sv) {
+            if (msg_name != "protobuf_unittest.TestMap"sv && msg_name != "proto2_unittest.TestWellKnownTypes"sv) {
               // Map fields can reorder entries on read, so encoded bytes may differ.
               assert(non_owning_write == owning_write);
             }
