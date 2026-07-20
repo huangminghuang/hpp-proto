aa8dffd693b10f149a9af6727a56ec5c48899a4a
Huang-Ming Huang <huangming.huang@gmail.com>
2026-07-19 19:20:46 -0500

    Isolate standalone fuzz instrumentation

--- fuzz/fuzz_descriptor.sh
+++ fuzz/fuzz_descriptor.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+mkdir -p descriptor_corpus
 ./fuzz_descriptor descriptor_corpus descriptor_seed_corpus -dict=fuzz_descriptor.dict -rss_limit_mb=4096 -timeout=3600 -runs=100000000
-
-### Minimize a crash with:
+### For debugging use the following command
 ###   fuzz_descriptor --minimize-crash crash-xxxxxxxx
